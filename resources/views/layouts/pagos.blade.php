d260203248d46a6869f895eef7d5760edd2737aa
Alfredo Galvan <alfredogalvan.91@gmail.com>
2021-10-07 14:35:46 -0500

    Programa Seguro

--- resources/views/layouts/pagos.blade.php
+++ resources/views/layouts/pagos.blade.php
@@ -100,10 +100,14 @@
 
                                         <tr>
                                             <td>@if ($p->status == 'approved')
-                                                <span class="badge b0g-success float-end mt-1 text-white">Aprovado</span>
+                                                <span class="badge bg-success float-end mt-1 text-white">Aprovado</span>
 
                                             @elseif ($p->status == 'pending')
                                                 <span class="badge bg-warning float-end mt-1 text-white">   Pendiente </span>
+
+                                            @elseif ($p->status == 'failure')
+                                                <span class="badge bg-danger float-end mt-1 text-white">   Fallido/Cancelado </span>
+
                                             @endif</td>
                                             <td>{{ $p->payment_id }}</td>
                                             <td>{{ \Carbon\Carbon::parse($upago[0]->fechaVencimiento)->format('d M, Y') }}</td>
