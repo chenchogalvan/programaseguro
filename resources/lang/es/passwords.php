45bb8acbd088d06bc04c22774bcddbdf1e98db5c
Alfredo Galvan <alfredogalvan.91@gmail.com>
2021-10-20 19:25:36 -0500

    Programa Seguro

--- resources/lang/es/passwords.php
+++ resources/lang/es/passwords.php
@@ -14,7 +14,7 @@ return [
     */
 
     'reset' => '¡Su contraseña ha sido restablecida!',
-    'sent' => '¡Recordatorio de contraseña enviado!',
+    'sent' => '¡Te hemos enviado un correo electrónico con las instrucciones para restablecer tu contraseña!',
     'token' => 'Este token de restablecimiento de contraseña es inválido.',
     'user' => 'No se ha encontrado un usuario con esa dirección de correo.',
     'throttled' => 'Por favor espere antes de volver a intentarlo.',
